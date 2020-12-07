088006e3fd35eb160c5923c644b2910fa1a2c7ed
John Yarham <yarhamjohn@gmail.com>
2020-12-07 19:55:23 +0000

    Day 3

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -10,8 +10,10 @@ namespace AdventOfCode
         {
             // Day1A(args[0]);
             // Day1B(args[0]);
-            Day2A(args[0]);
-            Day2B(args[0]);
+            // Day2A(args[0]);
+            // Day2B(args[0]);
+            Day3A(args[0]);
+            Day3B(args[0]);
         }
 
         private static void Day1A(string path)
@@ -40,6 +42,24 @@ namespace AdventOfCode
             var input = File.ReadLines(path);
             var total = Day2.CountValidPasswordsCorrectPolicy(input);
             Console.WriteLine(total);
+        }
+        
+        private static void Day3A(string path)
+        {
+            var input = File.ReadLines(path).ToList();
+            var total = Day3.CountTreesHit(input, 3, 1);
+            Console.WriteLine(total);
+        }
+        
+        private static void Day3B(string path)
+        {
+            var input = File.ReadLines(path).ToList();
+            var total1 = Day3.CountTreesHit(input, 1, 1);
+            var total2 = Day3.CountTreesHit(input, 3, 1);
+            var total3 = Day3.CountTreesHit(input, 5, 1);
+            var total4 = Day3.CountTreesHit(input, 7, 1);
+            var total5 = Day3.CountTreesHit(input, 1, 2);
+            Console.WriteLine((long)total1 * total2 * total3 * total4 * total5);
         }
         }
     }
