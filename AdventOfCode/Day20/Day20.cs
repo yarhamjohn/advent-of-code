42d04fcf059449a92f12892f39a7c8caf63b5132
John Yarham <yarhamjohn@gmail.com>
2020-12-25 19:20:21 +0000

    Day 20

---
+++ AdventOfCode/Day20/Day20.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class Day20
+    {
+        public static long SumCorners(List<string> input)
+        {
+            var tiles = GetTiles(input);
+            var tileEdges = GetTileEdges(tiles);
+            var cornerTileIds = GetCornerTiles(tileEdges);
+            return cornerTileIds.Aggregate(1L, (x, y) => x * y);
+        }
+
+        private static Dictionary<long, List<string>> GetTileEdges(Dictionary<long, List<string>> tiles)
+        {
+            var edges = new Dictionary<long, List<string>>();
+            foreach (var tile in tiles)
+            {
+                var listOfEdges = new List<string>
+                {
+                    tile.Value.First(),
+                    tile.Value.Last(),
+                    tile.Value.Select(x => x[^1].ToString()).Aggregate("", (x, y) => x + y),
+                    tile.Value.Select(x => x[^1].ToString()).Aggregate("", (x, y) => x + y)
+                };
+
+                edges[tile.Key] = listOfEdges;
+            }
+
+            return edges;
+        }
+
+        private static IEnumerable<long> GetCornerTiles(Dictionary<long, List<string>> tileEdges)
+        {
+            foreach (var (key, value) in tileEdges)
+            {
+                var edges = tileEdges.Where(x => x.Key != key).SelectMany(x => x.Value).ToList();
+                if (CountPairedEdges(value, edges) == 2)
+                {
+                    yield return key;
+                }
+            }
+        }
+
+        private static int CountPairedEdges(List<string> value, List<string> tileEdges)
+        {
+            return value.Count(x => !tileEdges.Contains(x) && !tileEdges.Contains(x.Reverse()));
+        }
+
+        private static Dictionary<long, List<string>> GetTiles(List<string> input)
+        {
+            var tiles = new Dictionary<long, List<string>>();
+
+            for (var i = 0; i < input.Count; i += 12)
+            {
+                var tileId = Convert.ToInt64(input[i].Split(" ")[1].Replace(":", "").Trim());
+                var tileContents = input.GetRange(i + 1, 10);
+                tiles[tileId] = tileContents;
+            }
+
+            return tiles;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Day20/Day20_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Day20
+{
+    [TestFixture]
+    public class Day20_Tests
+    {
+        [Test]
+        public void Should_Calculate_Result_Easy()
+        {
+            var inputPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Day20/Input/Example.txt");
+            var input = File.ReadAllLines(inputPath).ToList();
+            var result = AdventOfCode.Day20.SumCorners(input);
+            Assert.That(result, Is.EqualTo(20899048083289));
+        }
+    }
+}
