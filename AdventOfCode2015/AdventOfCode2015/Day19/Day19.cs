c416712be1ef8901342b84746289a34891f6ee28
John Yarham <yarhamjohn@gmail.com>
2022-09-16 16:56:35 +0100

    Day 19A

--- AdventOfCode2015/AdventOfCode2015/Day19/Day19.cs
+++ AdventOfCode2015/AdventOfCode2015/Day19/Day19.cs
@@ -6,33 +6,74 @@ public static class Day19
 {
     public static long CountDistinctMolecules(IEnumerable<string> input, string molecule)
     {
-        var replacements = input.Select(line => line.Split(" => ")).GroupBy(x => x[0]).ToArray();
+        var moleculeElements = GetMoleculeElements(molecule);
+        
+        var segments = input
+            .Select(line => line.Split(" => "))
+            .ToArray();
+            
+        var replacements = new Dictionary<string, List<string>>();
+
+        foreach (var elem in segments)
+        {
+            if (replacements.ContainsKey(elem[0]))
+            {
+                replacements[elem[0]].Add(elem[1]);
+            }
+            else
+            {
+                replacements[elem[0]] = new List<string> { elem[1] };
+            }
+        }
 
         var molecules = new List<string>();
 
-        for (var i = 0; i < molecule.Length; i++)
+        for (var i = 0; i < moleculeElements.Length; i++)
         {
-            var matchingGroup = replacements.Single(x => x.Key == molecule[i].ToString());
-            foreach (var item in matchingGroup)
+            var possibleReplacements = replacements.Where(x => x.Key == moleculeElements[i]).ToArray();
+            if (possibleReplacements.Length != 1)
+            {
+                continue;
+            }
+            
+            foreach (var repl in possibleReplacements.Single().Value)
             {
                 
                 var newMolecule = new StringBuilder();
-                newMolecule.Append(molecule[..i]);
-                newMolecule.Append(item[1]);
-                newMolecule.Append(molecule[(i + 1)..]);
+                newMolecule.Append(string.Join("", moleculeElements[..i]));
+                newMolecule.Append(repl);
+                newMolecule.Append(string.Join("", moleculeElements[(i + 1)..]));
                 
                 molecules.Add(newMolecule.ToString());
             }
         }
         
-        foreach (var group in replacements)
+        return molecules.Distinct().Count();
+    }
+
+    private static string[] GetMoleculeElements(string molecule)
+    {
+        var elements = new List<string>();
+
+        for (var i = 0; i < molecule.Length; i++)
         {
-            foreach (var item in group)
+            if (i == molecule.Length - 1)
             {
-                Console.WriteLine($"{group.Key}: {item[1]}");
+                elements.Add(molecule[i].ToString());
+                continue;
+            }
+
+            if (char.IsLower(molecule[i + 1]))
+            {
+                elements.Add(string.Join("", molecule[i..(i + 2)]));
+                i++;
+            }
+            else
+            {
+                elements.Add(molecule[i].ToString());
             }
         }
 
-        return molecules.Distinct().Count();
+        return elements.ToArray();
     }
 }
