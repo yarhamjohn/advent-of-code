bf370688718b046cb7b0837f1525aee6fe329a96
John Yarham <yarhamjohn@gmail.com>
2022-09-23 17:07:25 +0100

    wip day 19 b

--- AdventOfCode2015/AdventOfCode2015/Day19/Day19.cs
+++ AdventOfCode2015/AdventOfCode2015/Day19/Day19.cs
@@ -12,6 +12,16 @@ public static class Day19
             .Select(line => line.Split(" => "))
             .ToArray();
             
+        var replacements = GetReplacements(segments);
+
+        var molecules = GetMolecules(moleculeElements, replacements);
+
+        foreach (var m in molecules) Console.WriteLine(m);
+        return molecules.Distinct().Count();
+    }
+
+    private static Dictionary<string, List<string>> GetReplacements(string[][] segments)
+    {
         var replacements = new Dictionary<string, List<string>>();
 
         foreach (var elem in segments)
@@ -26,6 +36,11 @@ public static class Day19
             }
         }
 
+        return replacements;
+    }
+
+    private static List<string> GetMolecules(string[] moleculeElements, Dictionary<string, List<string>> replacements)
+    {
         var molecules = new List<string>();
 
         for (var i = 0; i < moleculeElements.Length; i++)
@@ -35,20 +50,112 @@ public static class Day19
             {
                 continue;
             }
-            
+
             foreach (var repl in possibleReplacements.Single().Value)
             {
-                
                 var newMolecule = new StringBuilder();
                 newMolecule.Append(string.Join("", moleculeElements[..i]));
                 newMolecule.Append(repl);
                 newMolecule.Append(string.Join("", moleculeElements[(i + 1)..]));
-                
+
                 molecules.Add(newMolecule.ToString());
             }
         }
+
+        return molecules;
+    }
+
+    public static long GetFewestSteps(IEnumerable<string> input, string molecule)
+    {
+        var segments = input
+            .Select(line => line.Split(" => "))
+            .ToArray();
+
+        var replacements = GetReplacements(segments);
+
+        var startElectrons = segments.Where(s => s[0] == "e");
+
+        var steps = int.MaxValue;
+
+        foreach (var electron in startElectrons)
+        {
+            var stepsNeeded = GetSteps(electron[1], replacements, molecule);
+
+            if (stepsNeeded < steps)
+            {
+                steps = stepsNeeded;
+            }
+        }
         
-        return molecules.Distinct().Count();
+        return steps;
+    }
+
+    private static int GetSteps(string startMolecule, Dictionary<string, List<string>> replacements, string targetMolecule)
+    {
+        var steps = 1;
+
+        var molecules = new List<string> { startMolecule};
+
+        while (true)
+        {
+            Console.WriteLine(steps);
+            Console.WriteLine(molecules.Count);
+            
+            // foreach (var m in molecules) Console.WriteLine(m);
+            
+            molecules = molecules.SelectMany(x => GetMolecules(GetMoleculeElements(x), replacements)).ToList();
+            // Console.WriteLine("----");
+
+            // foreach (var m in molecules) Console.WriteLine(m);
+            Console.WriteLine(molecules.Count);
+            // Console.WriteLine("----");
+
+            
+            // TODO Probably need to search deep via recursion rather than broad here
+                molecules = molecules
+                .Where(x => GetMoleculeElements(x).Last() != "Ar" && GetMoleculeElements(x).Last() != "Y" && GetMoleculeElements(x).Last() != "C" && GetMoleculeElements(x).Last() != "Rn")
+                .Where(x => 
+                    !x.Contains("YY") && 
+                    !x.Contains("YAr") && 
+                    !x.Contains("ArY") && 
+                    !x.Contains("RnRn") && 
+                    !x.Contains("RnY") && 
+                    !x.Contains("YRn") && 
+                    !x.Contains("ArRn") && 
+                    !x.Contains("RnAr") && 
+                    !x.Contains("CAr") && 
+                    !x.Contains("CC") && 
+                    !x.Contains("CY") &&
+                    !x.Contains("ArCRn") &&
+                    !x.Contains("RnCRn")) 
+                // ArAr is possible
+                // YC is possible
+                // CRn not at front
+                // ArCa RnCa is possible but not ArC or RnC
+                .Distinct()
+                .ToList();
+                
+                // foreach (var m in molecules) Console.WriteLine(m);
+
+                Console.WriteLine(molecules.Count);
+                Console.WriteLine("----------------------------------------");
+
+            if (molecules.Contains(targetMolecule))
+            {
+                return ++steps;
+            }
+
+            if (molecules.All(x => x.Length >= targetMolecule.Length))
+            {
+                return int.MaxValue;
+            }
+            
+            steps++;
+
+            // if (steps == 3) break;
+        }
+
+        return steps;
     }
 
     private static string[] GetMoleculeElements(string molecule)
--- AdventOfCode2015/AdventOfCode2015Tests/Day19Tests/Day19Tests.cs
+++ AdventOfCode2015/AdventOfCode2015Tests/Day19Tests/Day19Tests.cs
@@ -17,4 +17,17 @@ public class Day19Tests
         
         Assert.That(result, Is.EqualTo(expected));
     }
+    
+    
+    [TestCase("HOH", 3)]
+    [TestCase("HOHOHO", 6)]
+    public static void GetFewestSteps(string molecule, int expected)
+    {
+        var inputPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Day19Tests/Input/example.txt");
+        var input = File.ReadLines(inputPath);
+
+        var result = Day19.GetFewestSteps(input, molecule);
+        
+        Assert.That(result, Is.EqualTo(expected));
+    }
 }
