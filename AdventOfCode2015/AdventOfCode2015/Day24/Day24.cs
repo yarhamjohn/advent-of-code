3e8f2c64558df887a69eec54e300370bd704339e
John Yarham <yarhamjohn@gmail.com>
2022-10-28 14:07:45 +0100

    wip

--- AdventOfCode2015/AdventOfCode2015/Day24/Day24.cs
+++ AdventOfCode2015/AdventOfCode2015/Day24/Day24.cs
@@ -9,22 +9,129 @@ public static class Day24
         var totalWeight = packages.Sum();
         var groupWeight = totalWeight / 3;
 
-        for (var i = 0; i < packages.Length; i++)
+        var result = int.MaxValue;
+        
+        for (var combinationLength = 1; combinationLength <= packages.Length; combinationLength++)
         {
             // get all combinations of length i
+            var combinations = GetUniqueCombinations(packages, combinationLength);
+            
+            //Console.WriteLine("First Combinations");
+            //foreach (var a in combinations) Console.WriteLine(string.Join(",", a));
+            
             // filter for any combinations have required weight.
+            var validCombinations = combinations.Where(x => x.Sum() == groupWeight).ToArray();
+            
+            //Console.WriteLine("First Valid Combinations");
+            //foreach (var a in validCombinations) Console.WriteLine(string.Join(",", a));
+
             // if there are none move to next i
+            if (!validCombinations.Any())
+            {
+                continue;
+            }
+
             // if combinations do exist, repeat on remaining packages
+            foreach (var combination in validCombinations)
+            {
+                var qe = combination.Aggregate((a, b) => a * b);
+                if (qe > result)
+                {
+                    continue;
+                }
+
+                // Console.WriteLine(string.Join(",", combination));
+                var remainingPackages = packages.Where(x => !combination.Contains(x)).ToArray();
+                for (var secondCombinationLength = 1;
+                     secondCombinationLength <= remainingPackages.Length;
+                     secondCombinationLength++)
+                {
+                    // get all combinations of length i
+                    var secondCombinations = GetUniqueCombinations(remainingPackages, secondCombinationLength);
+
+                    //Console.WriteLine("Second Combinations");
+                    //foreach (var a in secondCombinations) Console.WriteLine(string.Join(",", a));
+ 
+                    // filter for any combinations have required weight.
+                    var validSecondCombinations = secondCombinations.Where(x => x.Sum() == groupWeight).ToArray();
+
+                    //Console.WriteLine("Second Valid Combinations");
+                    //foreach (var a in validSecondCombinations) Console.WriteLine(string.Join(",", a));
+                    
+                    // if there are none move to next i
+                    if (!validSecondCombinations.Any())
+                    {
+                        continue;
+                    }
+
+                    // if combinations do exist, repeat on remaining packages
+                    foreach (var comb in validSecondCombinations)
+                    {
+                        // Console.Write($"\r{string.Join(",", comb)}                               ");
+                        var lastPackages = remainingPackages.Where(x => !comb.Contains(x)).ToArray();
+                        for (var thirdCombinationLength = 1;
+                             thirdCombinationLength <= lastPackages.Length;
+                             thirdCombinationLength++)
+                        {
+                            // get all combinations of length i
+                            var thirdCombinations = GetUniqueCombinations(lastPackages, thirdCombinationLength);
+
+                            //Console.WriteLine("Third Combinations");
+                            //foreach (var a in thirdCombinations) Console.WriteLine(string.Join(",", a));
+
+                            // filter for any combinations have required weight.
+                            var validThirdCombinations = thirdCombinations.Where(x => x.Sum() == groupWeight).ToArray();
+
+                            //Console.WriteLine("Third Valid Combinations");
+                            //foreach (var a in validThirdCombinations) Console.WriteLine(string.Join(",", a));
+                            
+                            // if there are none move to next i
+                            if (!validThirdCombinations.Any())
+                            {
+                                continue;
+                            }
+
+                            // Console.WriteLine();
+                            // Console.Write($"\r{string.Join(",", validThirdCombinations.Select(x => string.Join("; ", x)))}                               ");
+
+                            foreach (var x in validThirdCombinations)
+                            {
+                                var quantumEntanglement = combination.Aggregate((a, b) => a * b);
+                                if (quantumEntanglement < result)
+                                {
+                                    result = quantumEntanglement;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (result < int.MaxValue) break;
+
             // if additional valid combinations are found, save them
             // otherwise, move to next i
         }
 
+        // foreach (var a in result)
+        // {
+        //     foreach (var b in a)
+        //     {
+        //         Console.WriteLine(string.Join(",", b));
+        //     }
+        //
+        //     Console.WriteLine("--------------------");
+        // }
+        return result;
+        // var fewestGroup1Packages = result.SelectMany(x => x.Select(y => y.ToList())).ToList();
+        // return GetQuantumEntanglement(fewestGroup1Packages).Min();
+
         // Get all possible combinations of 3 groups that each weigh the same as the groupWeight
-        var groupCombinations = GetGroupCombinations(packages, groupWeight);
+        //var groupCombinations = GetGroupCombinations(packages, groupWeight);
 
-        var fewestGroupOnePackages = GetFewestPackages(groupCombinations.Select(x => x.First().ToList()).ToList());
+        //var fewestGroupOnePackages = GetFewestPackages(groupCombinations.Select(x => x.First().ToList()).ToList());
 
-        return GetQuantumEntanglement(fewestGroupOnePackages).Min();
+        //return GetQuantumEntanglement(fewestGroupOnePackages).Min();
     }
 
     private static IEnumerable<int> GetQuantumEntanglement(List<List<int>> fewestGroup1Packages)
@@ -105,9 +212,9 @@ public static class Day24
 
     private static IEnumerable<int[]> GetUniqueCombinations(int[] packages, int length)
     {
-        for (var i = 0; i < packages.Length; i++)
+        for (var position = 0; position < packages.Length; position++)
         {
-            var package = packages[i];
+            var package = packages[position];
 
             if (length == 1)
             {
@@ -115,7 +222,7 @@ public static class Day24
             }
             else
             {
-                var combinations = GetUniqueCombinations(packages.Skip(i + 1).ToArray(), length - 1);
+                var combinations = GetUniqueCombinations(packages.Skip(position + 1).ToArray(), length - 1);
                 foreach (var result in combinations)
                 {
                     yield return new[] { package }.Concat(result).ToArray();
