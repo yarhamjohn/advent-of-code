50be7823bac96bf34880ea3935ea4e9f4d062706
John Yarham <yarhamjohn@gmail.com>
2022-11-02 20:50:54 +0000

    day 3b

--- AdventOfCode2016/AdventOfCode2016/Day3/Day3.cs
+++ AdventOfCode2016/AdventOfCode2016/Day3/Day3.cs
@@ -2,6 +2,29 @@
 
 public static class Day3
 {
+    public static int CountPossibleTrianglesVertically(IEnumerable<string> input)
+    {
+        var chunks = input
+            .Select(triangle => triangle.Split(" ")
+                .Where(x => x.Length != 0)
+                .Select(x => Convert.ToInt32(x))
+                .ToArray())
+            .Chunk(3);
+
+        var count = 0;
+        foreach (var chunk in chunks) {
+            for (var i = 0; i < chunk[0].Length; i++)
+            {
+                if (IsValidTriangle(new[] { chunk[0][i], chunk[1][i], chunk[2][i] }))
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+    
     public static int CountPossibleTriangles(IEnumerable<string> input)
     {
         return input
--- AdventOfCode2016/AdventOfCode2016/Program.cs
+++ AdventOfCode2016/AdventOfCode2016/Program.cs
@@ -10,5 +10,6 @@ string Day2A() => Day2.GetBathroomCode(File.ReadAllLines("./Day2/input.txt"));
 string Day2B() => Day2.GetExtendedBathroomCode(File.ReadAllLines("./Day2/input.txt"));
 
 long Day3A() => Day3.CountPossibleTriangles(File.ReadAllLines("./Day3/input.txt"));
+long Day3B() => Day3.CountPossibleTrianglesVertically(File.ReadAllLines("./Day3/input.txt"));
 
-Console.WriteLine(Day3A());
+Console.WriteLine(Day3B());
