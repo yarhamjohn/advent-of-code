0ecc8a88ee25fe3d9763f36be201b800b3b49efd
John Yarham <yarhamjohn@gmail.com>
2022-11-08 20:55:35 +0000

    day 10a

--- AdventOfCode2016/AdventOfCode2016/Day10/Day10.cs
+++ AdventOfCode2016/AdventOfCode2016/Day10/Day10.cs
@@ -4,14 +4,74 @@ public static class Day10
 {
     public static long GetComparerBotNumber(string[] input, int valueOne, int valueTwo)
     {
-        var bots = CreateBots(input.Where(x => x[..3] == "bot"));
+        var (bots, outputs) = CreateTargets(input.Where(x => x[..3] == "bot"));
 
+        ApplyInputs(input, bots);
+
+        while (bots.Values.Any(x => x.Active()))
+        {
+            
+            foreach (var bot in bots)
+            {
+                Console.WriteLine($"Bot: {bot.Key}, {bot.Value.ToString()}");
+            }
+            foreach (var output in outputs)
+            {
+                Console.WriteLine($"Output: {output.Key}, {output.Value.ToString()}");
+            }
+
+            Console.WriteLine("---------------------------");
+
+            var activeBots = bots.Values.Where(x => x.Active());
+            foreach (var bot in activeBots)
+            {
+                var instructions = bot.GetInstructions();
+            
+                if (!instructions.Select(x => x.value).Except(new [] {valueOne, valueTwo}).Any())
+                {
+                    return bot.Id;
+                }
+
+                foreach (var instruction in instructions)
+                {
+                    if (instruction.instruction is ToBot botInstruction)
+                    {
+                        bots[botInstruction.BotId].AddValue(instruction.value);
+                    }
+                
+                    if (instruction.instruction is ToOutput outputInstruction)
+                    {
+                        outputs[outputInstruction.BinId].AddValue(instruction.value);
+                    }
+                }
+            }
+        }
+
+        foreach (var bot in bots)
+        {
+            Console.WriteLine($"Bot: {bot.Key}, {bot.Value.ToString()}");
+        }
+        foreach (var output in outputs)
+        {
+            Console.WriteLine($"Bot: {output.Key}, {output.Value.ToString()}");
+        }
+        
         return 0;
     }
 
-    private static Dictionary<int, Target> CreateBots(IEnumerable<string> input)
+    private static void ApplyInputs(IEnumerable<string> input, IReadOnlyDictionary<int, Bot> bots)
     {
-        var targets = new Dictionary<int, Target>();
+        foreach (var line in input.Where(x => x[..5] == "value"))
+        {
+            var segments = line.Split(" ");
+            bots[Convert.ToInt32(segments[5])].AddValue(Convert.ToInt32(segments[1]));
+        }
+    }
+
+    private static (Dictionary<int, Bot>, Dictionary<int, Output>) CreateTargets(IEnumerable<string> input)
+    {
+        var bots = new Dictionary<int, Bot>();
+        var outputs = new Dictionary<int, Output>();
         foreach (var line in input)
         {
             var segments = line.Split(" ");
@@ -23,20 +83,20 @@ public static class Day10
                 ? new ToBot(Convert.ToInt32(segments[11]))
                 : new ToOutput(Convert.ToInt32(segments[11]));
 
-            targets.Add(Convert.ToInt32(segments[1]), new Bot(lowInstruction, highInstruction));
+            bots.Add(Convert.ToInt32(segments[1]), new Bot(Convert.ToInt32(segments[1]), lowInstruction, highInstruction));
 
             if (lowInstruction is ToOutput toOutputLow)
             {
-                targets.Add(Convert.ToInt32(toOutputLow.BinId), new Output());
+                outputs.Add(Convert.ToInt32(toOutputLow.BinId), new Output(Convert.ToInt32(toOutputLow.BinId)));
             }
             
             if (highInstruction is ToOutput toOutputHigh)
             {
-                targets.Add(Convert.ToInt32(toOutputHigh.BinId), new Output());
+                outputs.Add(Convert.ToInt32(toOutputHigh.BinId), new Output(Convert.ToInt32(toOutputHigh.BinId)));
             }
         }
 
-        return targets;
+        return (bots, outputs);
     }
 
     private interface Target
@@ -45,12 +105,14 @@ public static class Day10
 
     private class Bot : Target
     {
+        public int Id;
         private readonly List<int> _value = new();
         private readonly Instruction _lowInstruction;
         private readonly Instruction _highInstruction;
 
-        public Bot(Instruction lowInstructionInstruction, Instruction highInstructionInstruction)
+        public Bot(int id, Instruction lowInstructionInstruction, Instruction highInstructionInstruction)
         {
+            Id = id;
             _lowInstruction = lowInstructionInstruction;
             _highInstruction = highInstructionInstruction;
         }
@@ -65,14 +127,45 @@ public static class Day10
             _value.Add(value);
         }
 
+        public bool Active()
+        {
+            return _value.Count == 2;
+        }
+
         public (Instruction instruction, int value)[] GetInstructions()
         {
-            return new[] { (_lowInstruction, _value.Min()), (_highInstruction, _value.Max()) };
+            var instructions = new[] { (_lowInstruction, _value.Min()), (_highInstruction, _value.Max()) };
+
+            _value.RemoveAll(_ => true);
+            
+            return instructions;
+        }
+
+        public override string ToString()
+        {
+            return $"Values: {string.Join(",", _value)}";
         }
     }
 
     private class Output : Target
     {
+        private readonly List<int> _value = new();
+        public int Id;
+
+        public Output(int id)
+        {
+            Id = id;
+        }
+        
+        public void AddValue(int value)
+        {
+            _value.Add(value);
+        }
+        
+        public override string ToString()
+        {
+            return $"Values: {string.Join(",", _value)}";
+        }
     }
 
     private record Instruction;
---
+++ AdventOfCode2016/AdventOfCode2016Tests/Day10Tests/Day10Tests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode2016.Day10;
+using AdventOfCode2016.Day2;
+using NUnit.Framework;
+
+namespace AdventOfCode2016Tests.Day10Tests;
+
+[TestFixture]
+public class Day10Tests
+{
+    [Test]
+    public void GetTargetBot()
+    {
+        var inputPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Day10Tests/Input/example.txt");
+        var input = File.ReadLines(inputPath).ToArray();
+
+        var result = Day10.GetComparerBotNumber(input, 5, 2);
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
