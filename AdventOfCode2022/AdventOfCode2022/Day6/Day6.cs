b9798115455406a1b466f7e43bd54322f408e8ea
John Yarham <yarhamjohn@gmail.com>
2022-12-06 08:37:51 +0000

    2022 day 6a

---
+++ AdventOfCode2022/AdventOfCode2022/Day6/Day6.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2022.Day6;
+
+public static class Day6
+{
+    public static long GetStartOfPacket(string input)
+    {
+        var queue = new Queue<char>();
+
+        var position = 0;
+        while (true)
+        {
+            if (queue.Count == 4)
+            {
+                if (queue.ToArray().Distinct().Count() == 4)
+                {
+                    return position;
+                }
+
+                queue.Dequeue();
+            }
+            
+            queue.Enqueue(input[position]);
+
+            position++;
+        }
+    }
+}
---
+++ AdventOfCode2022/AdventOfCode2022Tests/Day6Tests/Day6Tests.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode2022.Day6;
+
+namespace AdventOfCode2022Tests.Day6Tests;
+
+[TestFixture]
+public class Day6Tests
+{
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void GetStartOfPacket(string input, int expected)
+    {
+        var result = Day6.GetStartOfPacket(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
