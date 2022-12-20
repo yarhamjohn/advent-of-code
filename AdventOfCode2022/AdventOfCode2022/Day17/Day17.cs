821f3fdd52570eb0a2ce9d9248632ea1822fc1dc
John Yarham <yarhamjohn@gmail.com>
2022-12-20 14:02:22 +0000

    2022 day 17

--- AdventOfCode2022/AdventOfCode2022/Day17/Day17.cs
+++ AdventOfCode2022/AdventOfCode2022/Day17/Day17.cs
@@ -2,106 +2,93 @@
 
 public static class Day17
 {
+    // Rolling window from last complete row
+    
+    
     public static long CalculateHeight(string input)
     {
-        var maxHeightOfRocks = 2022 / 5 * 13 + 4; // Because 2022 % 5 is 2, and the first 2 rocks have a possible height of 4
-        var maxHeightOfGrid = maxHeightOfRocks + 3 + 4; // New rocks appear 3 lines above and tallest rock is 4.
-
-        // 0 being just off the floor
-        var grid = Enumerable.Range(0, maxHeightOfGrid).Select(_ => Enumerable.Range(0, 7).Select(_ => ".").ToList()).ToList();
+        var grid = Enumerable.Range(0, GetMaxHeightOfGrid()).Select(_ => Enumerable.Range(0, 7).Select(_ => ".").ToList()).ToList();
         
-        // PrintGrid(grid);
-
         var jetIndex = 0;
         
         for (var i = 0; i < 2022; i++)
         {
-            var rockPositions = GetRockPositions(grid, i);
-            AddToGrid(grid, rockPositions);
-            // PrintGrid(grid);
+            var rockPositions = AddNewRockToGrid(grid, i);
 
-            if (CanMoveDirection(input[jetIndex], grid, rockPositions))
-            {
-                rockPositions = MoveDirection(input[jetIndex], grid, rockPositions);
-                // PrintGrid(grid);
-
-                jetIndex++;
-
-                if (jetIndex >= input.Length)
-                {
-                    jetIndex = 0;
-                }
-            }
-            else
-            {
-                // PrintGrid(grid);
-            }
+            jetIndex = HandleJetBlast(input, jetIndex, grid, ref rockPositions);
 
             while (true)
             {
                 if (CanMoveDown(grid, rockPositions))
                 {
                     rockPositions = MoveDown(grid, rockPositions);
-                    // PrintGrid(grid);
                 }
                 else
                 {
-                    UpdateGrid(grid, rockPositions);
-                    // PrintGrid(grid);
+                    foreach (var rock in rockPositions)
+                    {
+                        FixRockPosition(grid, rock);
+                    }
+
                     break;
                 }
 
-                if (CanMoveDirection(input[jetIndex], grid, rockPositions))
-                {
-                    rockPositions = MoveDirection(input[jetIndex], grid, rockPositions);
-                }
-
-                // PrintGrid(grid);
-
-                jetIndex++;
-
-                if (jetIndex >= input.Length)
-                {
-                    jetIndex = 0;
-                }
+                jetIndex = HandleJetBlast(input, jetIndex, grid, ref rockPositions);
             }
         }
         
         return GetStartRow(grid) - 3;
     }
 
-    private static void AddToGrid(List<List<string>> grid, List<(int row, int col)> rockPositions)
+    private static int HandleJetBlast(string input, int jetIndex, List<List<string>> grid, ref List<(int row, int col)> rockPositions)
     {
+        if (CanMoveDirection(input[jetIndex], grid, rockPositions))
+        {
+            rockPositions = MoveDirection(input[jetIndex], grid, rockPositions);
+        }
+
+        return ++jetIndex >= input.Length ? 0 : jetIndex;
+    }
+
+    private static int GetMaxHeightOfGrid()
+    {
+        const int maxHeightOfOneSetOfRocks = 13;
+        const int maxHeightOfAllRocks = 2022 / 5 * maxHeightOfOneSetOfRocks + 4; // Because 2022 % 5 is 2, and the first 2 rocks have a possible height of 4
+        return maxHeightOfAllRocks + 3 + 4; // New rocks appear 3 lines above and tallest rock is 4.
+    }
+
+    private static List<(int row, int col)> AddNewRockToGrid(List<List<string>> grid, long iteration)
+    {
+        var rockPositions = GetRockStartingPositions(grid, iteration);
+        
         foreach (var rock in rockPositions)
         {
-            grid[rock.row][rock.col] = "@";
+            SetPositionToRock(grid, rock);
         }
+
+        return rockPositions;
     }
 
-    private static List<(int row, int col)> MoveDirection(char c, List<List<string>> grid, List<(int row, int col)> rockPositions)
+    private static void SetPositionToRock(List<List<string>> grid, (int row, int col) rock)
     {
-        if (c == '>')
-        {
-            var orderedPositions = rockPositions.OrderByDescending(x => x.col).ToArray();
-            for (var i = 0; i < orderedPositions.Length; i++)
-            {
-                grid[orderedPositions[i].row][orderedPositions[i].col] = ".";
-                grid[orderedPositions[i].row][orderedPositions[i].col + 1] = "@";
-            }
-            
-            return rockPositions.Select(x => (x.row, x.col + 1)).ToList();
-        }
-
-        var orderedPositionsTwo = rockPositions.OrderBy(x => x.col).ToArray();
-        for (var i = 0; i < orderedPositionsTwo.Length; i++)
-        {
-            grid[orderedPositionsTwo[i].row][orderedPositionsTwo[i].col] = ".";
-            grid[orderedPositionsTwo[i].row][orderedPositionsTwo[i].col - 1] = "@";
-        }
-            
-        return rockPositions.Select(x => (x.row, x.col - 1)).ToList();
+        grid[rock.row][rock.col] = "@";
+    }
+    
+    private static void FixRockPosition(List<List<string>> grid, (int row, int col) rock)
+    {
+        grid[rock.row][rock.col] = "#";
+    }
+    
+    private static void SetPositionToEmpty(List<List<string>> grid, (int row, int col) rock)
+    {
+        grid[rock.row][rock.col] = ".";
     }
 
+    private static bool CanMoveDown(List<List<string>> grid, List<(int row, int col)> rockPositions)
+    {
+        return rockPositions.All(x => x.row != 0 && grid[x.row - 1][x.col] != "#");
+    }
+    
     private static bool CanMoveDirection(char c, List<List<string>> grid, List<(int row, int col)> rockPositions)
     {
         return c switch
@@ -114,77 +101,107 @@ public static class Day17
         };
     }
 
-    private static void UpdateGrid(List<List<string>> grid, List<(int row, int col)> rockPositions)
-    {
-        foreach (var rock in rockPositions)
-        {
-            grid[rock.row][rock.col] = "#";
-        }
-    }
-
     private static List<(int row, int col)> MoveDown(List<List<string>> grid, List<(int row, int col)> rockPositions)
     {
         var orderedPositions = rockPositions.OrderBy(x => x.row).ToArray();
         for (var i = 0; i < orderedPositions.Length; i++)
         {
-            grid[orderedPositions[i].row][orderedPositions[i].col] = ".";
-            grid[orderedPositions[i].row - 1][orderedPositions[i].col] = "@";
+            SetPositionToEmpty(grid, orderedPositions[i]);
+            SetPositionToRock(grid, (orderedPositions[i].row - 1, orderedPositions[i].col));
         }
 
         return rockPositions.Select(x => (x.row - 1, x.col)).ToList();
     }
 
-    private static bool CanMoveDown(List<List<string>> grid, List<(int row, int col)> rockPositions)
+    private static List<(int row, int col)> MoveDirection(char c, List<List<string>> grid, List<(int row, int col)> rockPositions)
     {
-        if (rockPositions.Any(x => x.row == 0))
+        if (c == '>')
         {
-            return false;
+            var orderedPositions = rockPositions.OrderByDescending(x => x.col).ToArray();
+            for (var i = 0; i < orderedPositions.Length; i++)
+            {
+                SetPositionToEmpty(grid, orderedPositions[i]);
+                SetPositionToRock(grid, (orderedPositions[i].row, orderedPositions[i].col + 1));
+            }
+            
+            return rockPositions.Select(x => (x.row, x.col + 1)).ToList();
         }
-        
-        return rockPositions.All(x => grid[x.row - 1][x.col] == "." || grid[x.row - 1][x.col] == "@");
+
+        var orderedPositionsTwo = rockPositions.OrderBy(x => x.col).ToArray();
+        for (var i = 0; i < orderedPositionsTwo.Length; i++)
+        {
+            SetPositionToEmpty(grid, orderedPositionsTwo[i]);
+            SetPositionToRock(grid, (orderedPositionsTwo[i].row, orderedPositionsTwo[i].col - 1));
+        }
+            
+        return rockPositions.Select(x => (x.row, x.col - 1)).ToList();
     }
 
-    private static List<(int row, int col)> GetRockPositions(List<List<string>> grid, int iteration)
+    private static List<(int row, int col)> GetRockStartingPositions(List<List<string>> grid, long iteration)
     {
-        var startCol = 2;
+        const int startCol = 2;
         var row = GetStartRow(grid);
 
-        switch (iteration % 5)
+        return (iteration % 5) switch
         {
-            case 0:
-                // ####
-                return new List<(int row, int col)> { (row, startCol), (row, startCol + 1), (row, startCol + 2), (row, startCol + 3) };
-            case 1: 
+            0 =>
+                /*
+                 * ####
+                 */
+                new List<(int row, int col)>
+                {
+                    (row, startCol), (row, startCol + 1), (row, startCol + 2), (row, startCol + 3)
+                },
+            1 =>
                 /*
                  * .#.
                  * ###
                  * .#.
                  */
-                return new List<(int row, int col)> { (row + 1, startCol), (row, startCol + 1), (row + 1, startCol + 1), (row + 2, startCol + 1), (row + 1, startCol + 2) };
-            case 2: 
+                new List<(int row, int col)>
+                {
+                    (row + 1, startCol),
+                    (row, startCol + 1),
+                    (row + 1, startCol + 1),
+                    (row + 2, startCol + 1),
+                    (row + 1, startCol + 2)
+                },
+            2 =>
                 /*
                  * ..#
                  * ..#
                  * ###
                  */
-                return new List<(int row, int col)> { (row, startCol), (row, startCol + 1), (row, startCol + 2), (row + 1, startCol + 2), (row + 2, startCol + 2) };
-            case 3: 
+                new List<(int row, int col)>
+                {
+                    (row, startCol),
+                    (row, startCol + 1),
+                    (row, startCol + 2),
+                    (row + 1, startCol + 2),
+                    (row + 2, startCol + 2)
+                },
+            3 =>
                 /*
                  * #
                  * #
                  * #
                  * #
                  */
-                return new List<(int row, int col)> { (row, startCol), (row + 1, startCol), (row + 2, startCol), (row + 3, startCol) };
-            case 4: 
+                new List<(int row, int col)>
+                {
+                    (row, startCol), (row + 1, startCol), (row + 2, startCol), (row + 3, startCol)
+                },
+            4 =>
                 /*
                  * ##
                  * ##
                  */
-                return new List<(int row, int col)> { (row, startCol), (row + 1, startCol), (row, startCol + 1), (row + 1, startCol + 1) };
-            default:
-                throw new InvalidOperationException("Not a valid rock selection");
-        }
+                new List<(int row, int col)>
+                {
+                    (row, startCol), (row + 1, startCol), (row, startCol + 1), (row + 1, startCol + 1)
+                },
+            _ => throw new InvalidOperationException("Not a valid rock selection")
+        };
     }
 
     private static int GetStartRow(List<List<string>> grid)
