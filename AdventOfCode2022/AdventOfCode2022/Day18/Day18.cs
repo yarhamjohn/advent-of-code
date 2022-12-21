13c2da50e974e2e85a94f9177d04d24a71874b40
John Yarham <yarhamjohn@gmail.com>
2022-12-21 16:28:43 +0000

    2022 day 18

--- AdventOfCode2022/AdventOfCode2022/Day18/Day18.cs
+++ AdventOfCode2022/AdventOfCode2022/Day18/Day18.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace AdventOfCode2022.Day18;
 
@@ -6,11 +7,7 @@ public static class Day18
 {
     public static long CalculateSurfaceArea(string[] input)
     {
-        var cubes = input
-            .Select(line => line.Split(",").Select(coord => Convert.ToInt32(coord)).ToArray())
-            .Select(coordinates => (x: coordinates[0], y: coordinates[1], z: coordinates[2]))
-            .ToArray();
-
+        var cubes = ParseCubes(input);
 
         return cubes
             .Select(GetNeighbourCubes)
@@ -18,48 +15,44 @@ public static class Day18
             .Sum();
     }
 
-    private static (int, int, int)[] GetNeighbourCubes((int x, int y, int z) cube)
-    {
-        return new[]
-        {
-            (cube.x + 1, cube.y, cube.z),
-            (cube.x, cube.y + 1, cube.z),
-            (cube.x, cube.y, cube.z + 1),
-            (cube.x - 1, cube.y, cube.z),
-            (cube.x, cube.y - 1, cube.z),
-            (cube.x, cube.y, cube.z - 1)
-        };
-    }
-
     public static long CalculateExternalSurfaceArea(string[] input)
     {
-        var surfaceArea = CalculateSurfaceArea(input);
+        var cubes = ParseCubes(input);
+        var matrix = ConstructMatrix(cubes);
 
-        var cubes = input
+        MarkLavaCubes(cubes, matrix);
+        MarkExternalCubes(matrix, (0, 0, 0));
+        
+        PrintMatrix(matrix);
+
+        return CalculateSurfaceArea(input) - GetInternalSurfaceArea(matrix);
+    }
+
+    private static (int x, int y, int z)[] ParseCubes(string[] input)
+        => input
             .Select(line => line.Split(",").Select(coord => Convert.ToInt32(coord)).ToArray())
             .Select(coordinates => (x: coordinates[0], y: coordinates[1], z: coordinates[2]))
             .ToArray();
 
+    private static string[][][] ConstructMatrix((int x, int y, int z)[] cubes)
+    {
         var matrix =
             Enumerable.Range(0, cubes.Max(c => c.x) + 1)
                 .Select(_ => Enumerable.Range(0, cubes.Max(c => c.y) + 1)
-                    .Select(_ => Enumerable.Range(0, cubes.Max(c => c.z) + 1).Select(_ => ".").ToArray()).ToArray()).ToArray();
+                    .Select(_ => Enumerable.Range(0, cubes.Max(c => c.z) + 1).Select(_ => ".").ToArray()).ToArray())
+                .ToArray();
+        return matrix;
+    }
 
+    private static void MarkLavaCubes((int x, int y, int z)[] cubes, string[][][] matrix)
+    {
         foreach (var cube in cubes)
         {
             matrix[cube.x][cube.y][cube.z] = "#";
         }
-
-        SetExternalSurfaceArea(matrix, (0, 0, 0));
-        
-        PrintMatrix(matrix);
-        
-        var internalSurfaceArea = GetInternalSurfaceArea(matrix);
-        
-        return surfaceArea - internalSurfaceArea;
     }
 
-    private static void SetExternalSurfaceArea(string[][][] matrix, (int x, int y, int z) cube)
+    private static void MarkExternalCubes(string[][][] matrix, (int x, int y, int z) cube)
     {
         var neighbours = GetNeighbourCubes(cube);
         
@@ -77,13 +70,8 @@ public static class Day18
 
         foreach (var neighbour in nextNeighbours)
         {
-            SetExternalSurfaceArea(matrix, neighbour);
+            MarkExternalCubes(matrix, neighbour);
         }
-    }
-
-    private static bool InMatrix(string[][][] matrix, (int, int, int) c)
-    {
-        return c.Item1 >= 0 && c.Item2 >= 0 && c.Item3 >= 0 && c.Item1 < matrix.Length && c.Item2 < matrix[c.Item1].Length && c.Item3 < matrix[c.Item1][c.Item2].Length;
     }
 
     private static int GetInternalSurfaceArea(string[][][] matrix)
@@ -106,96 +94,53 @@ public static class Day18
         return internalSurfaceArea;
     }
 
-    private static bool IsExternal(string[][][] matrix, int x, int y, int z)
-    {
-        return !IsInternalInX(matrix, x, y, z) || !IsInternalInY(matrix, x, y, z) || !IsInternalInZ(matrix, x, y, z);
-    }
+    private static bool IsExternal(string[][][] matrix, int x, int y, int z) =>
+        !IsInternalInX(matrix, x, y, z) || !IsInternalInY(matrix, x, y, z) || !IsInternalInZ(matrix, x, y, z);
 
     private static bool IsInternalInX(string[][][] matrix, int x, int y, int z)
-    {
-        var internalOne = false;
-        for (var position = x; position >= 0; position--)
-        {
-            if (matrix[position][y][z] == "#")
-            {
-                internalOne = true;
-            }
-        }
-        
-        var internalTwo = false;
-        for (var position = x; position < matrix.Length; position++)
-        {
-            if (matrix[position][y][z] == "#")
-            {
-                internalTwo = true;
-            }
-        }
-
-        return internalOne && internalTwo;
-    }
+        => Enumerable.Range(0, x).Any(i => matrix[i][y][z] == "#") &&
+            Enumerable.Range(x, matrix.Length - x).Any(i => matrix[i][y][z] == "#");
 
     private static bool IsInternalInY(string[][][] matrix, int x, int y, int z)
-    {
-        var internalOne = false;
-        for (var position = y; position >= 0; position--)
-        {
-            if (matrix[x][position][z] == "#")
-            {
-                internalOne = true;
-            }
-        }
-        
-        
-        var internalTwo = false;
-        for (var position = y; position < matrix[x].Length; position++)
-        {
-            if (matrix[x][position][z] == "#")
-            {
-                internalTwo = true;
-            }
-        }
-
-        return internalOne && internalTwo;
-    }
+        => Enumerable.Range(0, y).Any(i => matrix[x][i][z] == "#") &&
+           Enumerable.Range(y, matrix[x].Length - y).Any(i => matrix[x][i][z] == "#");
 
     private static bool IsInternalInZ(string[][][] matrix, int x, int y, int z)
-    {
-        var internalOne = false;
-        for (var position = z; position >= 0; position--)
-        {
-            if (matrix[x][y][position] == "#")
-            {
-                internalOne = true;
-            }
-        }
-        
-        
-        var internalTwo = false;
-        for (var position = z; position < matrix[x][y].Length; position++)
-        {
-            if (matrix[x][y][position] == "#")
-            {
-                internalTwo = true;
-            }
-        }
-
-        return internalOne && internalTwo;
-    }
+        => Enumerable.Range(0, z).Any(i => matrix[x][y][i] == "#") &&
+           Enumerable.Range(z, matrix[x][y].Length - z).Any(i => matrix[x][y][i] == "#");
     
-    private static int CountNeighbours(string[][][] matrix, int x, int y, int z)
-    {
-        return GetNeighbourCubes((x, y, z)).Where(c => InMatrix(matrix, c)).Count(c => matrix[c.Item1][c.Item2][c.Item3] == "#");
-    }
+    private static int CountNeighbours(string[][][] matrix, int x, int y, int z) 
+        => GetNeighbourCubes((x, y, z))
+            .Count(c => InMatrix(matrix, c) && matrix[c.Item1][c.Item2][c.Item3] == "#");
+    
+    private static (int, int, int)[] GetNeighbourCubes((int x, int y, int z) cube)
+        => new[]
+        {
+            (cube.x + 1, cube.y, cube.z),
+            (cube.x, cube.y + 1, cube.z),
+            (cube.x, cube.y, cube.z + 1),
+            (cube.x - 1, cube.y, cube.z),
+            (cube.x, cube.y - 1, cube.z),
+            (cube.x, cube.y, cube.z - 1)
+        };
 
+    private static bool InMatrix(string[][][] matrix, (int x, int y, int z) cube)
+        => cube.x >= 0
+           && cube.x < matrix.Length
+           && cube.y >= 0
+           && cube.y < matrix[cube.x].Length
+           && cube.z >= 0
+           && cube.z < matrix[cube.x][cube.y].Length;
+    
     private static void PrintMatrix(string[][][] matrix)
     {
-        for (var x = 0; x < matrix.Length; x++)
+        foreach (var x in matrix)
         {
-            for (var y = 0; y < matrix[x].Length; y++)
+            foreach (var y in x)
             {
-                for (var z = 0; z < matrix[x][y].Length; z++)
+                foreach (var z in y)
                 {
-                    Console.Write(matrix[x][y][z]);
+                    Console.Write(z);
                 }
 
                 Console.WriteLine();
