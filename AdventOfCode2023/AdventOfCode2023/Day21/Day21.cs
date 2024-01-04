9c79024ca90fe5952c834b1902e023a7b73686cf
John Yarham <yarhamjohn@gmail.com>
2024-01-04 22:29:21 +0000

    wip to try something different - doesn't look great so far :/

--- AdventOfCode2023/AdventOfCode2023/Day21/Day21.cs
+++ AdventOfCode2023/AdventOfCode2023/Day21/Day21.cs
@@ -25,11 +25,26 @@ public static class Day21
             var gridMapping = new Dictionary<(int x, int y), HashSet<(int x, int y)>>();
             // Then get relative next positions for all the values (this can be calculated once and stored)
             // Apply the relative positioning to each value - this may mean moving an element from one grid to another
-            
-            currentPositions = currentPositions.SelectMany(x => GetNextPositionsBig(x, grid)).ToHashSet();
+            foreach (var (key, value) in currentPositions)
+            {
+                // Next position contains a (row, col) which represents relative grid location and (x, y) which represents in-grid location
+                var nextPositions = value.SelectMany(x => GetNextPositionsBig(x, grid));
+                foreach (var pos in nextPositions)
+                {
+                    var newKey = (key.x + pos.row, key.y + pos.col);
+                    if (!gridMapping.ContainsKey(newKey))
+                    {
+                        gridMapping[newKey] = [];
+                    }
+                    
+                    gridMapping[newKey].Add((pos.x, pos.y));
+                }
+            }
+
+            currentPositions = gridMapping;
         }
         
-        return currentPositions.Count;
+        return currentPositions.Values.SelectMany(x => x).Count();
     }
     
     private static (List<List<Element>> grid, HashSet<(int x, int y)> currentPositions) ParseInput(string[] input)
@@ -47,6 +62,30 @@ public static class Day21
                 if (input[i][j] == 'S')
                 {
                     currentPositions.Add((i, j));
+                }
+            }
+            
+            grid.Add(row);
+        }
+
+        return (grid, currentPositions);
+    }
+
+    private static (List<List<Element>> grid, Dictionary<(int x, int y), HashSet<(int x, int y)>> currentPositions) ParseInputBig(string[] input)
+    {
+        var grid = new List<List<Element>>();
+        var currentPositions = new Dictionary<(int x, int y), HashSet<(int x, int y)>>();
+        
+        for (var i = 0; i < input.Length; i++) 
+        {
+            var row = new List<Element>();
+            for (var j = 0; j < input[0].Length; j++)
+            {
+                row.Add(input[i][j] == '#' ? Element.Rock : Element.Garden);
+
+                if (input[i][j] == 'S')
+                {
+                    currentPositions[(0, 0)] = [(i, j)];
                 }
             }
             
@@ -79,9 +118,9 @@ public static class Day21
         }
     }
 
-    private static readonly Dictionary<(int x, int y), List<(int x, int y)>> NextPositions = [];
+    private static readonly Dictionary<(int x, int y), List<(int row, int col, int x, int y)>> NextPositions = [];
     
-    private static IEnumerable<(int x, int y)> GetNextPositionsBig((int x, int y) position, List<List<Element>> grid)
+    private static IEnumerable<(int row, int col, int x, int y)> GetNextPositionsBig((int x, int y) position, List<List<Element>> grid)
     {
         if (NextPositions.TryGetValue(position, out var positions))
         {
@@ -89,7 +128,7 @@ public static class Day21
         }
 
         NextPositions[position] = [];
-
+        
         var numRows = (decimal) grid.Count;
         var numCols = (decimal) grid[0].Count;
 
@@ -104,22 +143,22 @@ public static class Day21
         
         if (grid[xModAbove][yMod] == Element.Garden)
         {
-            NextPositions[position].Add((position.x - 1, position.y));
+            NextPositions[position].Add((position.x == 0 ? -1 : 0, 0, position.x - 1, position.y));
         }
 
         if (grid[xMod][yModLeft] == Element.Garden)
         {
-            NextPositions[position].Add((position.x, position.y - 1));
+            NextPositions[position].Add((0, position.y == 0 ? -1 : 0, position.x, position.y - 1));
         }
         
         if (grid[xModBelow][yMod] == Element.Garden)
         {
-            NextPositions[position].Add((position.x + 1, position.y));
+            NextPositions[position].Add((position.x == numRows - 1 ? 1 : 0, 0, position.x + 1, position.y));
         }
         
         if (grid[xMod][yModRight] == Element.Garden)
         {
-            NextPositions[position].Add((position.x, position.y + 1));
+            NextPositions[position].Add((0, position.y == numCols - 1 ? 1 : 0, position.x, position.y + 1));
         }
         
         return NextPositions[position];
